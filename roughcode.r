8ad4bfe258a81e66c09f8f8809a766f70b92c8c6
Diceman01 <pierre.mccann@gmail.com>
2015-05-23 15:02:52 -0600

    Question 4 rough code

---
+++ roughcode.r
@@ -0,0 +1,12 @@
+library(dplyr)
+library(tidyr)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+cd <- SCC[grep("Coal",SCC$Short.Name),]
+ccd <- cd[grep("Comb",cd$Short.Name),]
+q4 <- inner_join(NEI,ccd, by = "SCC")
+q4_ans <- summarize(group_by(q4,year),total_emissions = sum(Emissions))
+barplot(q4_ans$total_emissions, names.arg = q4_ans$year, las = 1, main = "Coal-combustion related emissions for US")
+
